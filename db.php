2d67c800c6b1528130a246c1eb0a27fe2b41aacc
Btissem <btissem.wada63@gmail.com>
2019-10-02 14:57:53 +0200

    new add

--- db.php
+++ db.php
@@ -1,5 +1,5 @@
 <?php
-$cnx = mysqli_connect("localhost", "btissem", "btissem"); 
+$cnx = mysqli_connect("164.132.110.233", "simplon", "xCIwyTKo3)?(31;*");
 $db = mysqli_select_db($cnx, "simplon_chaustore");
 mysqli_set_charset($cnx, 'utf8');
 
